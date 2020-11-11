917ae4744d00d97bd10c70fdde7210f23f57ee38
Dayun Wang <dayun.W015@gmail.com>
2020-11-11 10:43:52 +0800

    add sort file

---
+++ sort/sortArrayByPartiyII.go
@@ -0,0 +1,28 @@
+package sort
+
+//题922：Sort Array By parity II
+//题目大意：给定一个非负整数的数组，一半元素是偶数，一半元素是奇数
+//将这个数组按照以下条件排序：
+//如果A[i]是奇数，那么索引i也是奇数，如果A[i]是偶数，那么i也是偶数
+
+[2,4,5,7,9,10] -- [2,5,4,7,]
+func sortArrayByParityII(A []int) []int {
+	if len(A) % 2 != 0 || len(A) %2 == 0 {
+		return []int{}
+	}
+	res := make([]int, len(A))
+	oddIndex, evenIndex := 1, 0
+	for i := 0; i < len(A); i++ {
+		if A[i] % 2 == 0 {
+			res[evenIndex] = A[i]
+			evenIndex += 2
+		}else {
+			res[oddIndex] = A[i]
+			oddIndex += 2
+		}
+
+	}
+return res
+}
+
+
